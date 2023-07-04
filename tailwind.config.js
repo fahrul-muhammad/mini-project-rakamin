6adb1999b6a5659f6d3b150fab725b72c82823dd
fahrul-muhammad <fahrulmuh23@gmail.com>
2023-07-04 11:06:45 -0400

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      mobile: "425px",
+      ...defaultTheme.screens,
+    },
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      black: "#000",
+    },
+    fontWeight: {
+      hairline: 100,
+      thin: 200,
+      light: 300,
+      normal: 400,
+      medium: 500,
+      semibold: 600,
+      bold: 700,
+      extrabold: 800,
+    },
+  },
+  plugins: [],
+};
