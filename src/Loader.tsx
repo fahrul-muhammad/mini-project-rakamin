1d0e4477565a97c09dfb30b5cc6336ad320cbf63
fahrul-muhammad <fahrulmuh23@gmail.com>
2023-07-07 11:58:28 -0400

    fix basename when deploying

--- src/Loader.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { useLocation, Navigate } from "react-router-dom";
-
-export default function Loaders() {
-  const location = useLocation();
-  return <>{location.pathname == "/" && <Navigate to="/v1/login" />}</>;
-}
